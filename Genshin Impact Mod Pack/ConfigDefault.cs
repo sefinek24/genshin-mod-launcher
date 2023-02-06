196542c8b7dd618caa922b71eaa868821f396a40
Sefinek <sefinek5916@gmail.com>
2023-02-06 04:05:19 +0100

    Update ConfigDefault.cs

--- Genshin Impact Mod Pack/ConfigDefault.cs
+++ Genshin Impact Mod Pack/ConfigDefault.cs
@@ -8,10 +8,10 @@ namespace Genshin_Impact_Mod
 		public const string AppId = "";
 		private const string Url = "https://discordapp.com/api/webhooks";
 
-		private static readonly bool DefaultChannel = !Regex.Match(Environment.UserName, "(?:sefinek)", RegexOptions.IgnoreCase | RegexOptions.Singleline).Success;
+		private static readonly bool Channel = !Regex.Match(Environment.UserName, "(?:sefinek)", RegexOptions.IgnoreCase | RegexOptions.Singleline).Success;
 
-		public static readonly string WebHookUrlDefault = DefaultChannel ? $"{Url}/" : $"{Url}/";
-		public static readonly string WebHookLogFiles = DefaultChannel ? $"{Url}/" : $"{Url}/";
-		public static readonly string WebHookUrlErrors = DefaultChannel ? $"{Url}/" : $"{Url}/";
+		public static readonly string WebHookUrlDefault = Channel ? $"{Url}/" : $"{Url}/";
+		public static readonly string WebHookLogFiles = Channel ? $"{Url}/" : $"{Url}/";
+		public static readonly string WebHookUrlErrors = Channel ? $"{Url}/" : $"{Url}/";
 	}
 }
